0097e430a53f1c33780e44340c01838f677cf791
yachitrasivakumar <yaya.s@hotmail.co.uk>
2023-12-03 22:45:21 +0000

    Added code to add method and length method in order to meet test requirements

--- src/main/java/com/example/ColourTable.java
+++ src/main/java/com/example/ColourTable.java
@@ -3,18 +3,33 @@ package com.example;
 import java.util.ArrayList;
 
 public class ColourTable {
-
-    int capacity;
+    int capacity = 0;
+    int max;
     ArrayList colours;
-
+    public ColourTable() {
+        throw new IllegalArgumentException("Must specify size of Colour Table");
+    }
     public ColourTable(int size) {
         if (size <= 1 || size >= 1025 || (size & (size - 1)) != 0) {
             throw new IllegalArgumentException("Palette size must be a power of two and between 2 and 1024.");
         }
-        this.capacity = size;
+        this.max = size;
         this.colours = new ArrayList<>(size);
     }
     public void add(int rgb) {
-        // add method functionality
+        if (colours.size() >= max) {
+            throw new IllegalArgumentException("Exceeded the capacity of the ColourTable.");
+        }
+        if ((rgb & 0xFF000000) != 0) {
+            throw new IllegalArgumentException("Invalid 24-bit RGB value.");
+        }
+        if (!colours.contains(rgb)) {
+            // only adds unique colours to palette
+            colours.add(rgb);
+            this.capacity += 1;
+        }
+    }
+    public int length(){
+        return this.capacity;
     }
 }
