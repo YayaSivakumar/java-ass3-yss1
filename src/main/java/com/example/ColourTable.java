89dd3d643789a1fda0695b5c27474a175b6bfe28
yachitrasivakumar <yaya.s@hotmail.co.uk>
2023-12-03 22:27:57 +0000

    Added code to testConstructorInvalidSize in order to meet test requirements

--- src/main/java/com/example/ColourTable.java
+++ src/main/java/com/example/ColourTable.java
@@ -1,9 +1,18 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class ColourTable {
 
+    int capacity;
+    ArrayList colours;
+
     public ColourTable(int size) {
-        // validation and initialization to be added after testing
+        if (size <= 1 || size >= 1025 || (size & (size - 1)) != 0) {
+            throw new IllegalArgumentException("Palette size must be a power of two and between 2 and 1024.");
+        }
+        this.capacity = size;
+        this.colours = new ArrayList<>(size);
     }
     public void add(int rgb) {
         // add method functionality
