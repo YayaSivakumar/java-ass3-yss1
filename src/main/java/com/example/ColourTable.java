e768aac01a0934708b81ab13722d89c58b86075d
yachitrasivakumar <yaya.s@hotmail.co.uk>
2023-12-03 22:15:43 +0000

    Created ColourTable class and defined test cases in ColourTableTest class

--- src/main/java/com/example/ColourTable.java
+++ src/main/java/com/example/ColourTable.java
@@ -2,9 +2,6 @@ package com.example;
 
 public class ColourTable {
 
-    public static void main (String args[]) {
-        System.out.println("hello");
-    }
     public ColourTable(int size) {
         // validation and initialization to be added after testing
     }
